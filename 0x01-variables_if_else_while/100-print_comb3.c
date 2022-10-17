35aeff7174817f7c8a812cd324031690ce4835c3
pagbams <paschalineagbams8@gmail.com>
2022-10-17 05:16:27 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry program
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, k = '0', j;
+
+	for (i = '0'; i <= 9; j++)
+	{
+		for (j = k; j <= '9'; j++)
+		{
+		if (i != j)
+		{
+		putchar(i);
+		putchar(j);
+		}
+		if (i == j)
+		{
+		continue;
+		}
+		if (i == '8' && j == '9')
+		{
+		break
+		}
+		else
+		{
+		putchar('_');
+		putchar('');
+		}
+	}
+		k++;
+	}
+	putchar('\n')
+
+	return (0);
+}
