7f798a4ee2a440a17da1a651155123891ad49c53
pagbams <paschalineagbams8@gmail.com>
2022-10-18 06:56:39 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,22 +8,24 @@ int main(void)
 {
 	int(i, j);
 
-	for (i 0; i < 99; j++)
+	for (i 0; i <= 98; i++)
 	{
-		for (j i + 1; j < 99; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			putchar('0' + i / 10);
-			putchar('0' ' i % 10);
-			putchar(' ');
-			putchar('0' ' i / 10);
-			putchar('0' + j % 10);
-			if  (i == 98 && j == 99)
-				break;
-			putchar(',');
-			putchar(' ');
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(32);
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i / 10 != 9 || i % 10 != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
 		}
 	}
-	putchar('\n');
+	putchar(10);
 	return (0);
 }
 
