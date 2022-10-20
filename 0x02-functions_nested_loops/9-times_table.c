ee7a90b12beba6df25d56a7764473497ef57a08b
pagbams <paschalineagbams8@gmail.com>
2022-10-20 08:09:20 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int num, multi, prod;
 
 	for (num = 0; num <= 9; num++)
 	{
@@ -16,15 +16,14 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			 prod = num + mult;
+			prod = num + mult;
 
-			 if (prod <= 9)
-				 _putchar(' ');
-			 else
-				 _putchar((prod / 10) + '0');
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
 
-
-			 _putchar((prod % 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
