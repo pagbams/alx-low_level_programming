32022da57ff8606c56b37b259b6338623c589c48
pagbams <paschalineagbams8@gmail.com>
2022-10-23 18:52:32 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * print_line - print a straight line using '-'
- * @n: the number of '-' to be printed
+ * print_line - printing line
+ * @n: the number of times
+ * Return: void
  */
 void print_line(int n)
 {
-	int len;
+	int i;
 
 	if (n > 0)
 	{
-		for (len = 0; len < n; len++)
-			_putchar('-');
+
+	for (i = 0; i < n; i++)
+		_putchar(' ');
 	}
 
-	_putchar('\n')
+	_putchar('\n');
+}
