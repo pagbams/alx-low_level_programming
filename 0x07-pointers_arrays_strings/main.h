2fd778548cf83a841b7ac839b1f09992f6164c50
pagbams <paschalineagbams8@gmail.com>
2022-10-31 21:25:38 -0700

    task 8

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 int _putchar(char c);
+void set_string(char **s, char *to);
 
 #endif
