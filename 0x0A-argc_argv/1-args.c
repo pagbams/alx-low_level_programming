6dff1f752db3d72f6b1041e43c740bfcd919d2ee
pagbams <paschalineagbams8@gmail.com>
2022-11-05 20:19:57 -0700

    argc used to identify the number of variables passed to the command

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -> this is the function to print its name
+ * @argc: arg parameter
+ * @argv: an array of a command listed
+ * return: 0 for success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n". argc - 1);
+	return (0);
+}
