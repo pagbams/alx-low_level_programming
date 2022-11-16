e8ff7623b2dddfcabfbe2a5fdf9286e9bce5b540
pagbams <paschalineagbams8@gmail.com>
2022-11-16 14:03:03 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,12 @@
 #define DOG
 
 /**
- * struct dog - dog class 
+ * struct dog - dog class
  *
  * @name: name of the dog
  *
  * @age: age of the dog
- * 
+ *
  * @owner: owner of the dog
  */
 struct dog
