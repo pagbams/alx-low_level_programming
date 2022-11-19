4779d9cdaea906128ca6e56d0eba5cee98eb44fd
pagbams <paschalineagbams8@gmail.com>
2022-11-19 11:59:34 -0800

    task 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 #include <stdio.h>
-#include <stdarg.h>                                                                                                                      
+#include <stdarg.h>
+
 int _putchar(char C);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
