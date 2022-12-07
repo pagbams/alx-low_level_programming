f587e1579d2f761bf4640c23293b815aed143f69
pagbams <paschalineagbams8@gmail.com>
2022-12-07 03:47:30 -0800

    task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * _ra - reallocates memory for an array of pointers 
- * to the nodes in a blinked list
+ * _ra - reallocates memory for an array of pointers
+ * to the nodes in a linked list
  * @list: the old list to append
  * @size: size of the new list (always one more than the old list)
  * @new: new node to add to the list
@@ -15,6 +15,7 @@ listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
 	listint_t **newlist;
 	size_t i;
+
 	newlist = malloc(size * sizeof(listint_t *));
 	if (newlist == NULL)
 	{
@@ -27,6 +28,7 @@ listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 	free(list);
 	return (newlist);
 }
+
 /**
  * free_listint_safe - frees a listint_t linked list
  * @head: double pointer to the start of the list
@@ -37,11 +39,28 @@ size_t free_listint_safe(listint_t **head)
 {
 	size_t i, num = 0;
 
-	listint_t *next = NULL;
+	listint_t **list = NULL;
 	listint_t *next;
 
-	if (hesd == NULL || *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (num);
 	while (*head != NULL)
 	{
-		if (*head != NULL)
+		for (i = 0; i < num; i++)
+		{
+			if (*head == list[i])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
+}
